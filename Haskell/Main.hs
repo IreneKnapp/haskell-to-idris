7a4254362f72b87f146adf5abad9ea4f6129ef90
Irene Knapp <irene.knapp@icloud.com>
2013-01-10 22:53:02 +0000

    Progress.  Produces stub output from real input.

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -98,5 +98,167 @@ visitModule :: HS.Module -> [HS.Comment] -> Conversion ()
 visitModule
     (HS.Module location name _ _ exports imports declarations) comments = do
   HS.ModuleName name <- return name
-  emit name
+  emit $ "Module " ++ name ++ "\n"
+  emit "\n"
+  mapM_ visitDeclaration declarations
+
+
+emitName :: HS.Name -> Conversion ()
+emitName (HS.Ident name) = emit name
+emitName (HS.Symbol name) = emit name
+
+
+emitModuleName :: HS.ModuleName -> Conversion ()
+emitModuleName (HS.ModuleName name) = emit name
+
+
+emitQName :: HS.QName -> Conversion ()
+emitQName (HS.Qual moduleName name) = do
+  emitModuleName moduleName
+  emit "."
+  emitName name
+emitQName (HS.UnQual name) = emitName name
+emitQName (HS.Special HS.UnitCon) = emit "()"
+emitQName (HS.Special HS.ListCon) = emit "[]"
+emitQName (HS.Special HS.FunCon) = emit "->"
+emitQName (HS.Special (HS.TupleCon _ n)) = do
+  emit "("
+  mapM_ (\_ -> emit ",") [0 .. n - 2]
+  emit ")"
+emitQName (HS.Special HS.Cons) = emit "::"
+emitQName (HS.Special HS.UnboxedSingleCon) = emit "()"
+
+
+visitDeclaration :: HS.Decl -> Conversion ()
+
+visitDeclaration
+    (HS.TypeDecl location name _ _) = do
+  emitName name
+  emit " : Type\n"
+  emit "\n"
+
+visitDeclaration
+    (HS.TypeFamDecl location name _ _) = do
+  emitName name
+  emit " ... : Type\n"
+  emit "\n"
+
+visitDeclaration
+    (HS.DataDecl location _ _ name _ _ _) = do
+  emit "data "
+  emitName name
+  emit "\n"
+  emit "  = ...\n"
+  emit "\n"
+
+visitDeclaration
+    (HS.GDataDecl location _ _ name _ _ _ _) = do
+  emit "data "
+  emitName name
+  emit "\n"
+  emit "  = ... : Type\n"
+  emit "\n"
+
+visitDeclaration
+    (HS.DataFamDecl location _ name _ _) = do
+  emit "data "
+  emitName name
+  emit "\n"
+  emit "  = ... : ... -> Type\n"
+  emit "\n"
+
+visitDeclaration
+    (HS.TypeInsDecl location _ _) = do
+  return ()
+
+visitDeclaration
+    (HS.DataInsDecl location _ _ _ _) = do
+  return ()
+
+visitDeclaration
+    (HS.GDataInsDecl location _ _ _ _ _) = do
+  return ()
+
+visitDeclaration
+    (HS.ClassDecl location _ name _ _ _) = do
+  emit "class "
+  emitName name
+  emit " where\n"
+  emit "\n"
+
+visitDeclaration
+    (HS.InstDecl location _ qname _ _) = do
+  return ()
+
+visitDeclaration
+    (HS.DerivDecl location _ qname _) = do
+  return ()
+
+visitDeclaration
+    (HS.InfixDecl location _ _ _) = do
+  return ()
+
+visitDeclaration
+    (HS.DefaultDecl location _) = do
+  return ()
+
+visitDeclaration
+    (HS.SpliceDecl location _) = do
+  return ()
+
+visitDeclaration
+    (HS.TypeSig location _ _) = do
+  return ()
+
+visitDeclaration
+    (HS.FunBind _) = do
+  return ()
+
+visitDeclaration
+    (HS.PatBind location _ _ _ _) = do
+  return ()
+
+visitDeclaration
+    (HS.ForImp location _ _ _ _ _) = do
+  return ()
+
+visitDeclaration
+    (HS.ForExp location _ _ _ _) = do
+  return ()
+
+visitDeclaration
+    (HS.RulePragmaDecl location _) = do
+  return ()
+
+visitDeclaration
+    (HS.DeprPragmaDecl location _) = do
+  return ()
+
+visitDeclaration
+    (HS.WarnPragmaDecl location _) = do
+  return ()
+
+visitDeclaration
+    (HS.InlineSig location _ _ qname) = do
+  return ()
+
+visitDeclaration
+    (HS.InlineConlikeSig location _ qname) = do
+  return ()
+
+visitDeclaration
+    (HS.SpecSig location qname _) = do
+  return ()
+
+visitDeclaration
+    (HS.SpecInlineSig location _ _ qname _) = do
+  return ()
+
+visitDeclaration
+    (HS.InstSig location _ qname _) = do
+  return ()
+
+visitDeclaration
+    (HS.AnnPragma location _) = do
+  return ()
 
