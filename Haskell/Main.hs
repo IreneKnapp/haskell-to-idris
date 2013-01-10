1fa445b5c66de4e640e74ef52d58b8aec1f72f6d
Irene Knapp <irene.knapp@icloud.com>
2013-01-10 20:45:06 +0000

    Broken who-knows-what, cold commit.

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -1,7 +1,26 @@
 module Main (main) where
 
+import qualified Language.Haskell.Exts as HS
+
 
 main :: IO ()
 main = do
-  putStrLn "Hello."
+  parameters <- getArgs
+  case parameters of
+    [inputPath, outputPath] -> do
+      convertFile inputPath outputPath
+
+
+convertFile :: FilePath -> FilePath -> IO ()
+convertFile inputPath outputPath = do
+  putStrLn $ "Converting " ++ inputPath ++ " to " ++ outputPath ++ "."
+  let parseMode = HS.defaultParseMode {
+		      HS.parseFilename = inputPath
+		    }
+  (parseResult, comments) <- HS.parseFileWithComments parseMode inputPath
+  case parseResult of
+    HS.ParseFailed location headline -> do
+      putStrLn $ "  Parse error at line " ++ HS.srcLine ++ ": " ++ headline
+    HS.ParseOk module -> do
+      putStrLn $ "  Converted."
 
